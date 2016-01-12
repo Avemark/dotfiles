8f2f02119370fed06644dd170fe5dc4553d78399
Christian Avemark <christian@avemark.se>
2016-01-12 10:43:13 +0100

    Add notifications!

--- bin/swap_keyboard_mapping.rb
+++ bin/swap_keyboard_mapping.rb
@@ -1,9 +1,17 @@
-#! /usr/bin/env ruby
+#! /home/christian/.rbenv/shims/ruby
+def notify(msg)
+  img = File.join File.dirname(__FILE__), 'keyboard.png'
+  `notify-send -i #{img} KeyboardSwapper "#{msg}"`
+end
+
+ALT_LAYOUT = 'tarmak2'
 
 if `xmodmap -pke | grep " 57" | cut -d '=' -f 2 | cut -c 2`.chomp == 'k'
   # we are running tarmak already
+  notify 'reset to qwerty'
   `xmodmap ~/.config/xmodmap/reset.xmodmap`
 else
   # we want to run tarmak
-  `xmodmap ~/.config/xmodmap/tarmak2.xmodmap`
+  notify "set to #{ALT_LAYOUT}"
+  `xmodmap ~/.config/xmodmap/#{ALT_LAYOUT}.xmodmap`
 end
