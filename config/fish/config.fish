76d43a5d428694aec62f0b35657f2a11db169ed0
Christian Avemark <christian@avemark.se>
2016-03-30 18:09:39 +0200

    changes!

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,8 +3,6 @@ set -x PATH $HOME/.rbenv/bin $PATH
 set -x PATH ./.git/safe/../../bin $PATH
 set -x PATH $HOME/.bin $PATH
 
-source ~/.git/git.fish
-source ~/.prompt/prompt.fish
-source ~/.ruby/ruby.fish
+source ~/.config/fish/prompt/prompt.fish
 
 source /usr/share/autojump/autojump.fish
--- config/fish/prompt/prompt.fish
+++ config/fish/prompt/prompt.fish
@@ -23,7 +23,7 @@ set __fish_git_prompt_color_untrackedfiles red
 set __fish_git_prompt_char_stashstate 's'
 set __fish_git_prompt_color_stashstate magenta
 set __fish_git_prompt_char_upstream_ahead ' ▲'
-set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream blue
 set __fish_git_prompt_char_upstream_behind ' ▼'
 set __fish_git_prompt_color_upstream_behind yellow
 set __fish_git_prompt_char_upstream_equal ' ='
@@ -35,6 +35,10 @@ set __fish_git_prompt_color_upstream_diverged red
 function fish_prompt
   set last_status $status
 
+  set git_prompt_string (__fish_git_prompt "$green⦗%s$green⦘")
+  if not test -z $git_prompt_string
+  	printf '%s\n' $git_prompt_string
+  end
   set_color $fish_color_cwd
   printf '%s' (prompt_pwd)
   set_color blue
@@ -45,7 +49,9 @@ function fish_prompt
 end
 
 function fish_right_prompt -d "print right hand prompt"
-	printf "%s" (__fish_git_prompt " git:%s")
+	if test $status -eq 1
+	  printf "%s" "$red!"
+	end
 end
 
 #  memo: ❰❱ ❬ ❭ ❮ ❯ ❯❯❯ ⧎ ⦗ ⦘「 」 『 』
